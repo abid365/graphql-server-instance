09bce2853e25e27a664d2d9cbffffad04aa32a14
jawad Bin Azam <abidibnazam@gmail.com>
2023-10-20 23:50:10 +0600

    created typeDefs

---
+++ index.js
@@ -0,0 +1,20 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// types
+import { typeDefs } from "./schema";
+
+// server setup
+const server = new ApolloServer({
+  // Apolloserver takes an objects as arguements
+  //   the object takes two properties
+  // *typeDefs -- definitions of types of data
+  // *resolvers
+  typeDefs,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at port`, 4000);
---
+++ schema.js
@@ -0,0 +1,30 @@
+export const typeDefs = `#graphql
+
+type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+}
+
+
+type Review {
+    id: ID!
+    review: Int!
+    content: String!
+}
+
+type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+}
+
+type Query {
+    reviews: [Reviews]
+    games: [Game]
+    authors: [Author]
+}
+
+`;
+
+// int , float, string, boolean, ID
